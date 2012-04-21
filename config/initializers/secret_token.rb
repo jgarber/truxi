f677d9f051924d4d59b5b545e43feed11f2b3196
Jason Garber <jg@jasongarber.com>
2012-04-21 06:14:04 -0400

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Truxi::Application.config.secret_token = '1635137810ef2c1016b3a66bacd1e3e231eeb96c69bdb6eed5aed49cce102a853ba0035880859d847b4a32312618a1f3c3a9936a71265a56dde3bff235ee9eba'
