658e0473ea075ac4ecf99b7bcc460ff06f3729e0
Jason Garber <jg@jasongarber.com>
2012-04-21 20:35:27 -0400

    Geocode and find distance to points

--- app/models/mover.rb
+++ app/models/mover.rb
@@ -3,12 +3,13 @@ class Mover < ActiveRecord::Base
   validates :user, :street_address, :city, :state, :zip, presence: true
   accepts_nested_attributes_for :user
 
-  acts_as_gmappable
-  geocoded_by :street_address
+  geocoded_by :gmaps4rails_address
+  after_validation :geocode
+  acts_as_gmappable address: "street_address", process_geocoding: false
 
   def miles_away_from(object)
     return unless object && object.latitude && object.longitude
-    sprintf "%.1f", self.distance_from([object.latitude, object.longitude])
+    sprintf "%.1f mi", self.distance_from([object.latitude, object.longitude])
   end
 
   def gmaps4rails_address
