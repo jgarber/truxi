8707f5e79676ec6980ab0082d053374ccb563dd3
Jason Garber <jg@jasongarber.com>
2012-04-21 20:23:10 -0400

    Add distance in the movers table

--- app/models/mover.rb
+++ app/models/mover.rb
@@ -4,6 +4,12 @@ class Mover < ActiveRecord::Base
   accepts_nested_attributes_for :user
 
   acts_as_gmappable
+  geocoded_by :street_address
+
+  def miles_away_from(object)
+    return unless object && object.latitude && object.longitude
+    sprintf "%.1f", self.distance_from([object.latitude, object.longitude])
+  end
 
   def gmaps4rails_address
   #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
