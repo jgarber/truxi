79aa29341c647f88e1f899c204a020edeb1379dc
Jason Garber <jg@jasongarber.com>
2012-04-22 15:10:28 -0400

    Beta page

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Truxi::Application.routes.draw do
   #   end
 
   match 'mover-signup' => 'welcome#mover_signup'
+  match 'beta' => 'welcome#beta'
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "welcome#index"
